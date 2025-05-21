0e157a976adc7a44466f77f46768cf10b713386d
Ambrogio Franzese <a.franzese@ecubit.it>
2025-05-21 10:40:02 +0200

    aggiustata estetica dei component

--- gameshop-FE/src/app/auth/auth.component.ts
+++ gameshop-FE/src/app/auth/auth.component.ts
@@ -1,12 +1,26 @@
-import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../service/auth.service';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
@@ -15,21 +29,19 @@ export class AuthComponent {
   password = '';
   errorMessage = '';
 
+  private readonly service = inject(AuthService);
+  private readonly snackBar = inject(MatSnackBar);
 
- private readonly service = inject(AuthService);
   onSubmit() {
-
-    this.service.login(this.username,this.password)
-      .subscribe({
-        next: (response) => {
-          console.log('Login response', response);
-          sessionStorage.setItem('isLoggedIn', 'true');
-          this.errorMessage = '';
-          window.location.href = '/videogames';
-        },
-        error: () => {
-          this.errorMessage = 'Credenziali non valide. Riprova';
-        },
-      });
+    this.service.login(this.username, this.password).subscribe({
+      next: (response) => {
+        sessionStorage.setItem('isLoggedIn', 'true');
+        this.errorMessage = '';
+        window.location.href = '/videogames';
+      },
+      error: () => {
+        this.errorMessage = 'Credenziali non valide. Riprova';
+      },
+    });
   }
 }
